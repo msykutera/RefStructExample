09fb94d246941b06bdd4d6c38967b19a604daddb
Michał Sykutera <msykutera@gmail.com>
2018-05-28 17:32:50 +0200

    Implementation

---
+++ RefStructExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RefStructExample
+{
+    public struct Example
+    {
+        public int Index { get; private set; }
+
+        public void IncreaseIndex() => Index += 1;
+    }
+
+    class Program
+    {
+        static void NonRefMethod(Example example) => example.IncreaseIndex();
+
+        static void RefMethod(ref Example example) => example.IncreaseIndex();
+
+        static void Main(string[] args)
+        {
+            var nonRefExample = new Example();
+            var refExample = new Example();
+
+            for (var i = 0; i < 10000000; i++)
+            {
+                NonRefMethod(nonRefExample);
+                RefMethod(ref refExample);
+            }
+
+            Console.WriteLine(nonRefExample.Index);
+            Console.WriteLine(refExample.Index);
+            Console.ReadLine();
+        }
+    }
+}
